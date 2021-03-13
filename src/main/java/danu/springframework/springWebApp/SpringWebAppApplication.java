9c3010d24bf15fc4b3d29b8507f00ded610340c6
Danu Atmaja <danuatmaja@gmail.com>
2021-03-13 15:53:27 +0800

    adding JPA example

---
+++ src/main/java/danu/springframework/springWebApp/SpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package danu.springframework.springWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppApplication.class, args);
+	}
+
+}
